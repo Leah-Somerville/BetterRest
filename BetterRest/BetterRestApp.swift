b1296ab20888b4989bfd8df64e257478daed7cb5
Leah Somerville <leah.somerville4@gmail.com>
2024-02-16 09:44:36 -0600

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Leah Somerville on 2/16/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
