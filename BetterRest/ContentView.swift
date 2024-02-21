4a27b2ea758287e456eba567800e2eed0e9c9866
Leah Somerville <leah.somerville4@gmail.com>
2024-02-21 10:43:16 -0600

    Finished Project

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -4,20 +4,114 @@
 //
 //  Created by Leah Somerville on 2/16/24.
 //
-
+import CoreML
 import SwiftUI
 
+
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+    @State private var wakeUp = defaultWakeTime
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? .now
     }
-}
+    
+    var body: some View {
+        NavigationStack{
+            Form{
+                Section{
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                }
+                Section {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                }
+                Section {
+                    Picker("Daily coffee intake", selection: $coffeeAmount) {
+                        ForEach(1..<20) {
+                            if $0 == 1 {
+                                Text("\($0) cup")
+                            } else {
+                                Text("\($0) cups")
+                            }
+                        }
+                    }.font(.headline)
+                }
+                
+                Section {
+                    Text(calculateBedtime())
+                        .multilineTextAlignment(.center)
+                        .frame(
+                            maxWidth: .infinity,
+                            maxHeight: .infinity,
+                            alignment: .center
+                        )
+                        .font(.title)
+                        .foregroundStyle(.blue)
+                        .padding()
+                    
+                } header: {
+                    Text("Recommended bed time").font(.title2).foregroundStyle(.black)
+                }
+                
+                
+            } // Form
+            .navigationTitle("BetterRest")
+            
+            
+        } // Nav Stack
+        
+    } // body
+    
+    
+    func calculateBedtime() -> String {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(
+                wake: Int64(Double(hour + minute)),
+                estimatedSleep: sleepAmount,
+                coffee: Int64(Double(coffeeAmount))
+            )
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            print(sleepTime)
+
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+
+
+            print(alertMessage)
+
+            return sleepTime.formatted(date: .omitted, time: .shortened)
+            
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+            return "Error"
+        }
+    }
+} // struct
 
 #Preview {
     ContentView()
